510b30a643a158e53a75afad0b3d84e4fc09720c
Aditya Chondke <aditya.chondke@gmail.com>
2023-08-30 13:31:33 +0530

    making this pokemon loader

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  FlatList,
+  Text,
+  useWindowDimensions,
+} from 'react-native';
+import pokemon from 'pokemontcgsdk';
+
+pokemon.configure({ apiKey: 'cacec0d9-7012-45ad-bddb-aa8aa78cb636' });
 
 export default function App() {
+  const windowWidth = useWindowDimensions().width;
+  const [pokemonCards, setPokemonCards] = useState([]);
+
+  useEffect(() => {
+    pokemon.card.where({ q: 'name:charizard'})
+      .then(result => {
+        setPokemonCards(result.data);
+      });
+  }, []);
+
+  const numColumns = windowWidth >= 768 ? 5 : 2; 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style={styles.title}> Charizard Pokémon Cards</Text>
+    <FlatList
+      data={pokemonCards}
+      keyExtractor={(item) => item.id.toString()}
+      numColumns={numColumns}
+      contentContainerStyle={styles.cardContainer}
+      renderItem={({ item }) => (
+        <View style={styles.card}>
+          <Image
+            source={{ uri: item.images.small }}
+            style={styles.cardImage}
+          />
+        </View>
+      )}
+    />
     </View>
   );
 }
@@ -14,7 +49,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 50,
+  },
+  card: {
+    flex: 1,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    borderRadius: 8,
+    padding: 16,
+    margin: 8,
+  },
+  cardImage: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'contain',
   },
 });
